d5b7f2fb36b30f1bae44d00f9471395a4f53ed61
Daria Anton <antonchenkodaria20@gmail.com>
2021-05-15 14:05:08 +0300

    add now.jsone, object, count and etc
    - добавление в разметку объекта style;
    - вывод значения переменной {};
    - вывод числа через {};
    - вывод результата арифметической операции через {};
    - вывод результата логической операции через {};
    - использование тернарного условия через {};
    - вывод undefined, null, false и true через {};
    - добавлен в корень проекта файл now.json;

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+const can = true;
+const word = 'Hi';
+const count = 11;
+const minus = 45 - 11
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello, new journey!</h1>
+        <h1 style = {{
+          color: 'blue'
+        }}>Hello, new journey!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {word} guest. You have done {count} lessons from {45}, else {minus} let's go!
+        </p>
+        <p>
+         you can do it {can && 'if is true'} {can ? 'if is true' : 'if is false'}
+          {undefined}  {null} {true} {false}
         </p>
         <a
           className="App-link"
