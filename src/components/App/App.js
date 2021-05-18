8c72f6c8840ed7759cfa792c569d598ae4b247ea
Daria Anton <antonchenkodaria20@gmail.com>
2021-05-18 21:51:15 +0300

    create 'components' inside folder 'scr'
    1. Внутри папки src мы создадим папку components, внутри которой будут храниться все наши компоненты.
    2. Для каждого компонента создается отдельная папка, внутри которой будут находиться также стили и картинки, относящиеся к этому компоненту.
    3. Название папки, файла и самого компонента должно совпадать.

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ItemList from '../ItemList/ItemList';
+
+const App = () => (
+    <div>
+        <h1>InputItem</h1>
+        <ItemList />
+        <h1>Footer</h1>
+    </div>);
+
+export default App;
