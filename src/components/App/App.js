d9e8f0845f015161f4deb21138cb638492ea113e
Daria Anton <antonchenkodaria20@gmail.com>
2021-05-21 18:35:27 +0300

    Создание списка дел в компоненте App и передача его в ItemList.
    1. Создание списка дел в компоненте App и передача его в ItemList.
    2. Внутри ItemList итерируем список и передаем значения дел в Item, который должен выводить сам текст дела.
    3. Добавление ключа к элементу списка.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,28 @@ import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 
-const todoItem = 'write new app';
+const todpItem = 'write new app'
 
-const App = () => (
+const App = () => {
+    const items = [
+        {
+            value: 'write new app'
+        },
+        {
+            value: 'write props'
+        },
+        {
+            value: 'will done all deals'
+        }
+    ];
+
+    return( 
     <div>
         <h1>Important deals</h1>
         <InputItem />
-        <ItemList todoItem={todoItem}/>
+        <ItemList items={items}/>
         <Footer count={3} />
     </div>);
+}
 
 export default App;
