570c137409dbbd867211fce24b6d6bc73a149a63
Daria Anton <antonchenkodaria20@gmail.com>
2021-05-24 08:20:30 +0300

    create App.css
    1. Подключение App.css к компоненту App и отцентровка основного блока приложения.
    
    2. Подключение шрифтов

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
+import './App.css';
 
 const todpItem = 'write new app'
 
@@ -19,8 +20,8 @@ const App = () => {
     ];
 
     return( 
-    <div>
-        <h1>Important deals</h1>
+    <div className="wrap">
+        <h1 className="wrap__title">Important deals</h1>
         <InputItem />
         <ItemList items={items}/>
         <Footer count={3} />
