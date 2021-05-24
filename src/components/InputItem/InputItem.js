8efdcc6d0f8fb248d89d85b19e41acddf4047910
Daria Anton <antonchenkodaria20@gmail.com>
2021-05-24 15:14:19 +0300

    install  Material UI
    1. Установка Material UI в проект.
    
    2. Подключение  готовых компонентов Material UI в проект

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<span>InputItem</span>)
+const InputItem = () => (<div>
+    <TextField 
+         id="outlined-disabled"
+         label="Input task"
+         variant="outlined"
+    />
+    </div>)
 
 export default InputItem;
