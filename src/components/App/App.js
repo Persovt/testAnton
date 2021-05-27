31c65c2fa7281fa80b0deed72deb550dbeda1433
Daria Anton <antonchenkodaria20@gmail.com>
2021-05-27 16:10:57 +0300

    checkbox on click
    При клике на чек-бокс или сам пункт, его состояние должно меняться с { isDone: false } на { isDone: true } и наоборот.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,19 +9,36 @@ class App extends React.Component {
         items: [
             {
                 value: 'write new app',
-                isDone: true
+                isDone: true,
+                id: 1
             },
             {
                 value: 'write props',
-                isDone: true
+                isDone: true,
+                id: 2
             },
             {
                 value: 'will done all deals',
-                isDone: false
+                isDone: false,
+                id: 3
             }
-        ]
+        ],
+        count: 1
     };
-onClickDone = isDone => console.log(isDone);
+    
+onClickDone = id => {
+    const newItemList = this.state.items.map(item => {
+        const newItem = { ...item};
+
+        if (item.id === id) {
+            newItem.isDone = !item.isDone;
+        }
+
+        return newItem;
+    });
+
+    this.setState({ items: newItemList})
+};
 
     render () {
         return( 
@@ -29,7 +46,7 @@ onClickDone = isDone => console.log(isDone);
                 <h1 className={styles.title}>Important deals</h1>
                 <InputItem />
                 <ItemList items={this.state.items} onClickDone={this.onClickDone} />
-                <Footer count={1} />
+                <Footer count={this.state.count} />
         </div>);
     }
 };
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import Item from '../Item/Item';
 import List from '@material-ui/core/List';
+import Item from '../Item/Item';
 
 const ItemList = ({ items, onClickDone }) => (<List>
   {items.map(item => <li key={item.value}>
-      <Item value={item.value} isDone={item.isDone} onClickDone={onClickDone} />
+      <Item 
+        value={item.value} 
+        isDone={item.isDone} 
+        id={items.id} 
+        onClickDone={onClickDone} 
+        />
   </li>)}
 </List>);
 
