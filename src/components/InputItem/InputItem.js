1f1ad59af21b6dee4b38c0861f1bc1fcc02d0398
Daria Anton <antonchenkodaria20@gmail.com>
2021-05-30 11:23:52 +0300

    refactoring use state hook in inputItem component

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,64 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import styles from './InputItem.module.css';
 
-class InputItem extends React.Component {
+const InputItem = ({
+  onClickAdd,
+}) => {
 
-    state = {
-        inputValue: '',
-        isError: false
-    };
-
-  onButtonClick = () => {
-     if (this.state.inputValue == false) {
-      this.setState({
-        isError: true
-      })
-    } else {
-      this.setState({
-        inputValue: '',
-        isError: false
-      })
-      this.props.onClickAdd(this.state.inputValue);
-    }
+const initialState =  {
+    textField: {
+      inputValue: '',
+      errorStatus: false,
+      helperText: ' ',
+    },
   };
 
-    render() {
-        let textField;
-        if (this.state.isError == false) {
-            textField = <TextField
-                id="standard-full-width"
-                placeholder="Type the name of the task"
-                fullWidth
-                value={this.state.inputValue}
-                isError={this.props.isError}
-                onChange={event => this.setState({inputValue: event.target.value})}
-            />
-        } else {
-            textField = <TextField
-                error
-                id="standard-full-width"
-                placeholder="The field musn't be empity"
-                fullWidth
-                value={this.state.inputValue}
-                isError={this.props.isError}
-                onChange={event => this.setState({inputValue: event.target.value})}
-            />
-        };
+  const [inputValue,setInputvalue,] = useState(initialState.textField.inputValue);
 
-        return (<Grid> { textField }
-            <Button
-                variant='contained'
-                color='#d3d3d3'
-                fullWidth
-                onClick={this.onButtonClick}
-                onClickAdd ={this.onClickAdd}
-                > 
+  const [errorStatus,setErrorStatus,] = useState(initialState.textField.errorStatus);
+
+  const [helperText,setHelperText,] = useState(initialState.textField.helperText);
+
+
+ const onButtonClick = () => {
+     setInputvalue('');
+     setErrorStatus(false);
+     setHelperText(' ');
+
+ if (inputValue) {
+      onClickAdd(inputValue);
+} else { setInputvalue('');
+    setErrorStatus(true);
+    setHelperText("The field must not be empity");
+  } 
+};
+
+ return (<Grid className={styles.wrapper}> 
+   <TextField
+      error={errorStatus}
+      helperText={helperText}
+      className={styles.textField}
+      fullWidth
+      label="Type the name of the task">
+    </TextField>
+           
+    <Button className={styles.button}
+        variant='contained'
+        fullWidth
+        onClick={onButtonClick}
+        onClickAdd={onClickAdd}> 
                 Add
-            </Button>
-            </Grid>);
-    }
+    </Button>
+  </Grid>);
 };
 
 export default InputItem;
