f3400097eaedae9a0550d49424f8c6bfcb7674e0
Daria Anton <antonchenkodaria20@gmail.com>
2021-05-30 19:18:23 +0300

    react-router-dom
    Подключение react-router-dom к приложению.
    Проведение рефакторинга приложения, создание меню и дополнительные компоненты для разделов "Обо мне" и "Контакты".
    Подключение и настрой роутинг в приложении.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,89 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import InputItem from '../InputItem/InputItem';
-import ItemList from '../ItemList/ItemList';
-import Footer from '../Footer/Footer';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Card from '@material-ui/core/Card'
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+
+import Todo from '../Todo/Todo';
+import About from '../About/About';
+import Contacts from '../Contacts/Contacts';
+
 import styles from './App.module.css';
 
-const App = () => {
-
-    const initialState = {
-        items: [
-            {
-                value: 'write new app',
-                isDone: true,
-                id: 1
-            },
-            {
-                value: 'write props',
-                isDone: true,
-                id: 2
-            },
-            {
-                value: 'will done all deals',
-                isDone: false,
-                id: 3
-            }
-        ],
-        count: 3
-    };
-
-    const [items, setItems] = useState(initialState.items);
-    const [count, setCount] = useState(initialState.count);
-
-    useEffect( () => {
-        console.log('update');
-      });
-      useEffect( () => {
-        console.log('mount');
-      }, []);
-
-const onClickDone = id => {
-    const newItemList = items.map(item => {
-        const newItem = { ...item};
-
-        if (item.id === id) {
-            newItem.isDone = !item.isDone;
-        }
-
-        return newItem;
-    });
-
-    setItems(newItemList);
-};
-
-const onClickDelete = id => {
-    const newItemList = items.filter (item => item.id !== id);
-    setItems(newItemList);
-        setCount (count - 1)
-};
-
-
-const onClickAdd = value => {
-    const newItemList = [
-        ...items,
-        {
-           value,
-           isDone: false,
-           id:count + 1
-        }        
-       ];
-   setItems(newItemList);
-     setCount(count + 1)
-};
-
-    return ( 
+const App = () => 
+   (<Router> 
       <div className={styles.wrap}>
-        <h1 className={styles.title}>Important deals</h1>
-         <InputItem 
-            onClickAdd={onClickAdd}
-            />
-            <ItemList 
-               items={items} 
-               onClickDone={onClickDone} 
-               onClickDelete={onClickDelete} />
-            <Footer count={count} />
-    </div>);
- }
-       
+      <Card className={styles.panel}>
+         <MenuList>
+         <Link to='/'className={styles.link}><MenuItem>About</MenuItem></Link>
+         <Link to='/todo'className={styles.link}><MenuItem>Todo</MenuItem></Link>
+         <Link to='/contacts'className={styles.link}><MenuItem>Contacts</MenuItem></Link>
+         </MenuList>
+      </Card>
+
+      <Card className={styles.content}>
+            <Route path='/' exact component={About} />
+            <Route path='/todo' component={Todo} />
+            <Route path='/contacts' component={Contacts} />
+      </Card>
+   </div>
+</Router>);
 
 export default App;
